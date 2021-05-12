9befe97a64bcd2a0dd408dcbfcd9e5ea585c311c
mengssui <1711900861@qq.com>
2021-05-12 17:13:47 +0800

    特性新增: 使用cmake管理

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+###
+ # @Descripttion: 
+ # @version: 0.1
+ # @Author: Mengbw
+ # @Date: 2021-05-12 16:57:02
+ # @LastEditors: Mengbw
+ # @LastEditTime: 2021-05-12 17:06:20
+### 
+set -x
+
+SOURCE_DIR=`pwd`
+BUILD_DIR=${BUILD_DIR:-../build}
+BUILD_TYPE=${BUILD_TYPE:-debug}
+INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install}
+BUILD_NO_EXAMPLE=${BUILD_NO_EXAMPLE:-0}
+
+mkdir -p $BUILD_DIR/BUILD_TYPE \
+  && cd $BUILD_DIR \
+  && cmake $SOURCE_DIR  \
+  && make $*
+
